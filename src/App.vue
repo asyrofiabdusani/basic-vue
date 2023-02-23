f883dd29e776ef9c11b94c836804917963d305b7
asyrofiabdusani <asyrofiabdusani@gmail.com>
2023-02-23 10:18:21 +0700

    binding element

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,28 @@
 <template>
   <div>{{greet}} {{ content }}</div>
   <div v-text="vText"></div>
+  <div v-html="vHtml"></div>
+  <div v-bind:id="headingId">Heading</div>
+  <button v-bind:disabled="isDisabled">BT Bind</button>
+  <h2 class="underline">Underline</h2>
+  <h2 v-bind:class="status">Binding Class</h2>
+  <h2 class="underline" v-bind:class="status">Combine binding and static class</h2>
+  <h2 v-bind:class="promoted && 'success'">Conditional binding</h2>
+  <h2 v-bind:class="isRun?'success':'danger'">Result</h2>
+  <h2 v-bind:class="['success', 'underline']">newly promoted</h2>
+  <h2 v-bind:class="[promoted && 'success', isRun?'danger':'underline']">newly promoted</h2>
+  <h2 v-bind:class="{
+    promoted: promoted,
+    new:isRun,
+    'success':!isRun
+  }">Object conditional</h2>
+  <h2 v-bind:style="{
+    color:primary, 
+    fontSize:size + 'px', 
+    'text-decoration': 'underline' 
+    }">Binding inline style</h2>
+  <h2 v-bind:style="headerStyle">Binding style</h2>
+  <h2 v-bind:style="[headerStyle, successStyle]">Binding style</h2>
 </template>
 
 <script>
@@ -11,7 +33,22 @@ export default {
     return {
       greet : 'Hello',
       name: 'World!',
-      vText: 'V-Text'
+      vText: 'V-Text',
+      vHtml: '<b>V-HTML</b>',
+      headingId: 'heading',
+      isDisabled: true,
+      status: 'danger',
+      promoted: true,
+      isRun: false,
+      primary: 'blue',
+      size: 20,
+      headerStyle: {
+        fontSize: '20px',
+        textDecoration: 'underline',
+      },
+      successStyle: {
+        color: 'green',
+      }
     }
   }
 }
@@ -25,5 +62,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.underline {
+  text-decoration: underline;
+}
+.danger {
+  color: red;
+}
+.success {
+  color: green;
 }
 </style>
